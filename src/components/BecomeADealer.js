4325134c545df581065114ead65f7e36e8c89971
mphoshabalala3401@gmail.com <mphoshabalala3401@gmail.com>
2023-12-03 00:59:16 +0200

    dealers side updated

--- src/components/BecomeADealer.js
+++ src/components/BecomeADealer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import network from "../images/network.png";
 export default function BecomeADealer({ rightArrow }) {
   return (
     <div className="flex-col sm:mt-8 bg-carImg bg-cover bg-center bg-no-repeat px-8 md:px-24 h-screen flex items-center text-gray-800 md:flex-row">
@@ -8,15 +9,15 @@ export default function BecomeADealer({ rightArrow }) {
           WORK WITH US, AND BECOME A DEALER
         </h1>
         <p className="md:text-2xl text-gray-600 font-semibold">
-          Let us help your business reach new heights, sign-up and become a
-          dealer.
+          Let us help your business reach new heights, sign-up and join a
+          network of like minded individuals.
         </p>
         <div className="border-blue-200 border-2 flex flex-col md:flex-row  justify-center  p-2 rounded mt-4 w-fit">
           <Link
             to="/dealer-form"
             className="bg-purple-800 px-4 py-2 md:py-4 md:px-8 md:text-2xl text-white font-semibold md:font-bold    rounded flex items-center"
           >
-            Become A dealer
+            JOIN NOW
             <span>
               <img
                 className="h-4 md:h-6 ml-2  md:ml-4"
@@ -27,7 +28,10 @@ export default function BecomeADealer({ rightArrow }) {
           </Link>
         </div>
       </div>
-      <div className="w-1/2"></div>
+      <div className="w-1/2">
+        {" "}
+        <img src={network} alt="" />
+      </div>
     </div>
   );
 }
